3787cdeeaeabd338242ee3d739e54160494f5319
shanthi1710 <shanthinathshedabale17@gmail.com>
2024-08-21 20:54:29 +0530

    setup

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import clsx from "clsx";
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
+}
